a4c08df6eebcb31eb0dfff9fe32989fe8ce3a42f
MattiasLarsenn <mattiaslarsen@hotmail.com>
2026-04-30 14:05:09 +0200

    project build

---
+++ src/main/java/org/example/biludlejning/BilUdlejningApplication.java
@@ -0,0 +1,14 @@
+package org.example.biludlejning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BilUdlejningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilUdlejningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/biludlejning/BilUdlejningApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.biludlejning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilUdlejningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
