58e8a6568da22ab5205c6e109f88af45c6deb073
luffe141 <luffeerder@gmail.com>
2026-05-09 17:32:46 +0200

    made costumer view

--- src/main/java/org/example/biludlejning/BilUdlejningApplication.java
+++ src/main/java/org/example/biludlejning/BilUdlejningApplication.java
@@ -1,11 +1,7 @@
 package org.example.biludlejning;
 
-import org.example.biludlejning.utility.ConnectionManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.datasource.AbstractDataSource;
-
-import javax.sql.DataSource;
 
 
 @SpringBootApplication
